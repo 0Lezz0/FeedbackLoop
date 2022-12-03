2f257757f0713ad9b17f49585f117e672557812b
0Lezz0 <43016588+0Lezz0@users.noreply.github.com>
2022-12-03 18:36:02 -0300

    WIP - Finished basic movement and look controls
    - Acceptable version of ground and air movement

--- FeedbackLoopUnity/Assets/Scripts/MechaScripts/Movement/MechaMovement.cs
+++ FeedbackLoopUnity/Assets/Scripts/MechaScripts/Movement/MechaMovement.cs
@@ -15,17 +15,26 @@ public class MechaMovement : MonoBehaviour
     [SerializeField]
     private float pitch;
     private bool shouldJumpOrDash;
+    private float verticalCameraRotation;
 
     [SerializeField]
     private bool _isFlying;
     [SerializeField]
-    private float _groundSpeed, _airSpeed;
+    private float _groundSpeed, _airSpeed, _verticalSpeed;
+    [SerializeField]
+    private float _verticalCameraLimitGround, _verticalCameraLimitAir;
     [SerializeField]
     private Camera _mainMechaCamera;
+    [SerializeField]
+    private Vector2 _mouseSensitivity;
     public bool IsFlying { get => _isFlying; set => _isFlying = value; }
     public float GroundSpeed { get => _groundSpeed; set => _groundSpeed = value; }
     public float AirSpeed { get => _airSpeed; set => _airSpeed = value; }
     public Camera MainMechaCamera { get => _mainMechaCamera; set => _mainMechaCamera = value; }
+    public Vector2 MouseSensitivity { get => _mouseSensitivity; set => _mouseSensitivity = value; }
+    public float VerticalSpeed { get => _verticalSpeed; set => _verticalSpeed = value; }
+    public float VerticalCameraLimitGround { get => _verticalCameraLimitGround; set => _verticalCameraLimitGround = value; }
+    public float VerticalCameraLimitAir { get => _verticalCameraLimitAir; set => _verticalCameraLimitAir = value; }
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +46,7 @@ public class MechaMovement : MonoBehaviour
         mechaRigidBody = gameObject.GetComponent<Rigidbody>();
 
         IsFlying = false;
+        verticalCameraRotation = 0f;
     }
 
     // Update is called once per frame
@@ -49,6 +59,8 @@ public class MechaMovement : MonoBehaviour
         {
             shouldJumpOrDash = true;
         }
+
+        AimCamera(mechaActions.Aim.ReadValue<Vector2>());
     }
     private void FixedUpdate()
     {
@@ -73,14 +85,22 @@ public class MechaMovement : MonoBehaviour
 
     private void MoveOnGround()
     {
-        Vector3 velocity = new Vector3(movement.x, 0, movement.y) * GroundSpeed;
+        Vector3 cameraForward = new(gameObject.transform.forward.x, 0, gameObject.transform.forward.z);
+        Vector3 cameraRight = new(gameObject.transform.right.x, 0, gameObject.transform.right.z);
+        Vector3 moveDirection = cameraForward.normalized * movement.y + cameraRight.normalized * movement.x;
+
+        Vector3 velocity = moveDirection * GroundSpeed;
 
         mechaRigidBody.AddForce(velocity, ForceMode.Impulse);
     }
 
     private void MoveOnAir()
     {
-        Vector3 velocity = new Vector3(movement.x, pitch, movement.y) * AirSpeed;
+        Vector3 cameraForward = new(gameObject.transform.forward.x, 0, gameObject.transform.forward.z);
+        Vector3 cameraRight = new(gameObject.transform.right.x, 0, gameObject.transform.right.z);
+        Vector3 moveDirection = cameraForward.normalized * movement.y + cameraRight.normalized * movement.x;
+
+        Vector3 velocity = new Vector3(moveDirection.x * AirSpeed, pitch * VerticalSpeed, moveDirection.z * AirSpeed);
 
         mechaRigidBody.AddForce(velocity, ForceMode.Impulse);
     }
@@ -98,5 +118,20 @@ public class MechaMovement : MonoBehaviour
     private void ToggleFlight()
     {
         IsFlying = !IsFlying;
+    }
+    private void AimCamera(Vector2 aimingPoint)
+    {
+
+        verticalCameraRotation -= (aimingPoint.y * Time.deltaTime) * MouseSensitivity.y;
+        if (IsFlying)
+        {
+            verticalCameraRotation = Mathf.Clamp(verticalCameraRotation, -1f * VerticalCameraLimitAir, VerticalCameraLimitAir);
+        }
+        else {
+            verticalCameraRotation = Mathf.Clamp(verticalCameraRotation, -1f * VerticalCameraLimitGround, VerticalCameraLimitGround);
+        }
+
+        MainMechaCamera.transform.localRotation = Quaternion.Euler(verticalCameraRotation, 0, 0);
+        transform.Rotate(Vector3.up * (aimingPoint.x * Time.deltaTime) * MouseSensitivity.x);
     }
 }
